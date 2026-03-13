83e401197a7196271802f5de469e9155d07e052e
Manisha Singh <manishasinghbcs89@gmail.com>
2026-03-14 02:20:16 +0530

    Worked on js topics and rename files.

---
+++ day_21_functiontest.js
@@ -0,0 +1,9 @@
+//worked on function how its worked in side the function and return the value to outside of the function.
+
+function Test(a) {
+  //let result = 2 * a;
+  return 2 * a;
+}
+//let output = Test(5);
+Test(5);
+console.log("Output is: " + Test(5));
